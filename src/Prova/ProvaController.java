7a6899766def51d05fe4b03176bb50defb237521
Pierluigi <Pierluigi@10.7.30.189>
2017-05-26 16:21:39 +0200

    Aggiungi bottone

--- src/Prova/ProvaController.java
+++ src/Prova/ProvaController.java
@@ -1,5 +1,30 @@
 package Prova;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ProvaController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnpress;
+
+    @FXML
+    void doPress(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnpress != null : "fx:id=\"btnpress\" was not injected: check your FXML file 'Prova.fxml'.";
+
+    }
 }
